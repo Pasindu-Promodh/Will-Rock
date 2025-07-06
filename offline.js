93a8803cdf0048993aca8c2ab2bcfddf0aa4e68b
Pasindu-Promodh <pasindugunathilaka96@gmail.com>
2025-07-07 01:15:09 +0530

    Initial commit

---
+++ offline.js
@@ -0,0 +1,33 @@
+﻿{
+	"version": 1735898069,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/backgroundnew-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/pickchris-sheet0.png",
+		"images/particles.png",
+		"images/trianglemorty-sheet0.png",
+		"images/trianglemorty-sheet1.png",
+		"images/particles2.png",
+		"images/pngmemegameswhatthefuck_-sheet0.png",
+		"images/assetsmemecubehowto-sheet0.png",
+		"images/play-sheet0.png",
+		"media/player jumps.m4a",
+		"media/player jumps.ogg",
+		"media/coin collect.m4a",
+		"media/coin collect.ogg",
+		"media/wilhelm scream.m4a",
+		"media/wilhelm scream.ogg",
+		"media/meme cube bgm.m4a",
+		"media/meme cube bgm.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
